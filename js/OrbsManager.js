dd470a95ec420a66f53339f8d31fcc7b2a384639
Florent Bélot <106475863+BelotFlorent@users.noreply.github.com>
2025-04-22 22:00:43 +0200

    Update OrbsManager.js

--- js/OrbsManager.js
+++ js/OrbsManager.js
@@ -7,12 +7,10 @@ export default class OrbsManager {
     createOrbsAtPositions(positions) {
         positions.forEach(pos => {
             BABYLON.SceneLoader.ImportMesh("", "./images/", "carotte.glb", this.scene, (meshes) => {
-                let orb = meshes[0]; // On suppose que le premier mesh est la carotte
+                let orb = meshes[0];
                 orb.position.set(pos.x, pos.y !== undefined ? pos.y : 1, pos.z);
-                orb.scaling = new BABYLON.Vector3(0.5, 0.5, 0.5); // Ajustez l'échelle si nécessaire
-
-                // ** Ombres **
-                orb.receiveShadows = true;                             // reçoit l’ombre
+                orb.scaling = new BABYLON.Vector3(0.5, 0.5, 0.5);
+                orb.receiveShadows = true;
 
 
                 this.orbs.push(orb);
@@ -20,9 +18,8 @@ export default class OrbsManager {
         });
     }
 
-    // Utilisation d'un test de distance plutôt que de intersectsMesh pour la détection de collision
     checkCollisions(player, onCollision) {
-        const collisionDistance = 3; // Seuil de distance pour récupérer une orbe
+        const collisionDistance = 3;
         for (let i = this.orbs.length - 1; i >= 0; i--) {
             let orb = this.orbs[i];
             let dist = BABYLON.Vector3.Distance(player.mesh.position, orb.position);
